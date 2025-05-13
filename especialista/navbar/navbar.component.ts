0e6ec46be9e1dcbbcd9a35e602b34985feabdb4a
Salvadorvga2004 <116208397+Salvadorvga2004@users.noreply.github.com>
2025-05-12 20:40:36 -0600

    Componente navbar

---
+++ especialista/navbar/navbar.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { Usuarios } from 'src/app/models/models';
+import { AuthService } from 'src/app/services/auth.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  isLoginPage: boolean = false;
+  isHomePage: boolean = false;
+  isLoggedIn: boolean = false;
+
+  usuarios: Usuarios[] = []
+
+
+  constructor(private router: Router, private authService: AuthService, private usuariosServ: UsuariosService) { }
+
+  ngOnInit() {
+   
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      const url = this.router.url;
+      this.isLoginPage = url === '/login';
+      this.isHomePage = url === '/home';
+    });
+
+    this.authService.getCurrentUser().subscribe((user: Usuarios | null) => {
+      this.isLoggedIn = !!user;
+      this.usuarios = user ? [user] : [];
+    });
+  }
+
+  
+
+
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/home']); // Redirige al componente Home después de cerrar sesión
+    this.isLoggedIn = false;
+  }
+
+
+  mouseEnter() {
+    if (window.innerWidth >= 992) {
+      // Solo activar hover en desktop
+      const dropdown = document.querySelector('.nav-item.dropdown');
+      dropdown?.classList.add('show');
+      const menu = dropdown?.querySelector('.dropdown-menu');
+      menu?.classList.add('show');
+    }
+  }
+  
+  mouseLeave() {
+    if (window.innerWidth >= 992) {
+      const dropdown = document.querySelector('.nav-item.dropdown');
+      dropdown?.classList.remove('show');
+      const menu = dropdown?.querySelector('.dropdown-menu');
+      menu?.classList.remove('show');
+    }
+  }
+
+}
